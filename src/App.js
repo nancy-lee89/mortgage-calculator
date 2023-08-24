a5160cf8a7d934440d0a651e2b05f3ea30206753
Nancy Lee <nramirez289@gmail.com>
2023-08-24 14:07:28 -0700

    created project folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import SliderSelect from "./components/SliderSelect";
+import TenureSelect from "./components/TenureSelect";
+import Result from "./components/Result";
 
 function App() {
-  return <div className="App">Hello Everyone</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <SliderSelect />
+      <TenureSelect />
+      <Result />
+    </div>
+  );
 }
 
 export default App;
