062b93158b25d5ea4e9961e33b4faee45b26f466
Nancy Lee <nramirez289@gmail.com>
2023-08-28 11:02:58 -0700

    Added props to slider component

--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 import SliderComponent from "./common/SliderComponent";
 
 const SliderSelect = () => {
-  return <SliderComponent />;
+  return (
+    <Fragment>
+      <SliderComponent min={0} max={50} defaultValue={20} />
+    </Fragment>
+  );
 };
 
 export default SliderSelect;
--- src/components/common/SliderComponent.js
+++ src/components/common/SliderComponent.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Slider from "@mui/material/Slider";
 
-const SliderComponent = () => {
+const SliderComponent = ({ defaultValue, min, max }) => {
   return (
-    <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+    <Slider
+      defaultValue={defaultValue}
+      min={min}
+      max={max}
+      aria-label="Default"
+      valueLabelDisplay="auto"
+    />
   );
 };
 
