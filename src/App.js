1bb88c7c75d36194ea89a2bc857ef5b917c443f8
Nancy Lee <nramirez289@gmail.com>
2024-01-25 11:08:26 -0800

    Added two other sliders and components to sliders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import SliderSelect from "./components/SliderSelect";
 import TenureSelect from "./components/TenureSelect";
@@ -7,9 +8,14 @@ import Container from "@mui/material/Container";
 import { Grid } from "@mui/material";
 
 function App() {
+  const [a, setA] = useState(15);
+
+  console.log(a);
+
   return (
     <div className="App">
       <Navbar />
+      <button onClick={() => setA(30)}>Click Me</button>
       <Container maxWidth="lg" sx={{ marginTop: 5 }}>
         <Grid container spacing={5}>
           <Grid item xs={12} md={6}>
--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -3,9 +3,38 @@ import SliderComponent from "./common/SliderComponent";
 
 const SliderSelect = () => {
   return (
-    <Fragment>
-      <SliderComponent min={0} max={50} defaultValue={20} />
-    </Fragment>
+    <>
+      <SliderComponent
+        label="Home Value"
+        min={0}
+        max={250}
+        defaultValue={20}
+        step={5}
+        onChange={(event) => console.log(event.target.value)}
+        unit="$"
+        amount={3000}
+      />
+      <SliderComponent
+        label="Down Payment"
+        min={0}
+        max={150}
+        defaultValue={20}
+        step={5}
+        onChange={(event) => console.log(event.target.value)}
+        unit="$"
+        amount={500}
+      />
+      <SliderComponent
+        label="Loan Amount"
+        min={0}
+        max={1250}
+        defaultValue={20}
+        step={5}
+        onChange={(event) => console.log(event.target.value)}
+        unit="$"
+        amount={700}
+      />
+    </>
   );
 };
 
